4e4f8d8fb32d20a01f79ef9a95686fa2b9e5e736
Shane O'Mahony <sleepymole@users.noreply.github.com>
2019-04-05 10:12:31 +0100

    added some changes to sample react code

--- src/App.js
+++ src/App.js
@@ -3,24 +3,65 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      msg: "Hello banner message!",
+      data: [
+        "First message",
+        "Second message",
+      ]
+    };
+    //necessary to have this work as expected
+    this.addData = this.addData.bind(this);
+  }
+
+  addData() {
+    let newData = this.state.data;
+    newData.push("this is a new line, new line, new line!");
+
+    this.setState({
+      data: newData
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HelloBanner msg={this.state.msg} data={this.state.data}/>
+
+        <input type="button" onClick={this.addData} value={"Click me!"}/>
       </div>
+    );
+  }
+}
+
+
+class HelloBanner extends Component {
+
+  render() {
+    return (
+        <div>
+          {this.props.msg}
+
+          {this.props.data.map((val, index) => {
+            return <ListItem key={index} txt={val}/>
+          })}
+        </div>
+    );
+  }
+}
+
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>{this.props.txt}</div>
     );
   }
 }
